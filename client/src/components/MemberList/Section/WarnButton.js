549951ced5591763ee1dd8f4b313c677e6925b52
k0502s <k0502s@naver.com>
2021-03-18 10:21:19 +0900

    회원 리스트 경고 버튼 구현

---
+++ client/src/components/MemberList/Section/WarnButton.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+import { useDispatch } from 'react-redux';
+import { MEMBER_WARN_REQUEST } from '../../../redux/types';
+
+const WarnButton = (props) => {
+    const dispatch = useDispatch();
+
+    const clickHandler = () => {
+        const body = {
+            token: localStorage.getItem('token'),
+            warndata: props.detail._id,
+        };
+        //필요한 정보를 Cart 필드에 넣어준다.
+        // dispatch(addToCart(props.detail._id))
+        dispatch({
+            type: MEMBER_WARN_REQUEST,
+            payload: body,
+        });
+    };
+
+    return (
+        <div>
+            <div>
+                <Button className="m-3 btn-sm btn-danger" onClick={clickHandler}>
+                    WARN
+                </Button>
+            </div>
+        </div>
+    );
+};
+
+export default WarnButton;
