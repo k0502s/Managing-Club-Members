e7766e153ded129126c9c1568f9ac3f0bc6beb2f
k0502s <k0502s@naver.com>
2021-04-13 18:41:36 +0900

    회원 추가 페이지 CSS 수정 및 추가
    회원 추가 참고 사항 카드 추가, 버튼에 아이콘 추가 및 CSS 수정, 레이아웃 수정, 일러스트 png 추가, 프로필 칸 위치 조정 및 아이콘 추가

--- client/src/components/Main/AddMemberPage/AddMemberPage.js
+++ client/src/components/Main/AddMemberPage/AddMemberPage.js
@@ -3,6 +3,8 @@ import { Button, Form, Input, Label, Card, CardTitle, CardText, Row, Col, CardHe
 import FileUpload from '../../../utils/FileUpload';
 import { useDispatch, useSelector } from 'react-redux';
 import { MEMBER_UPLOADING_REQUEST } from '../../../redux/types';
+import * as S from './AddMemberPage.style';
+import img from '../../../assets/img/imgbin_photography-logo-photographer-png.png';
 
 const Sex = [
     { key: 1, value: '남' },
@@ -75,39 +77,67 @@ const AddMemberPage = (props) => {
     };
 
     return (
-        <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
-            <div style={{ textAlign: 'center', marginBottom: '1rem' }}>
+        <>
+            {/* <div style={{ textAlign: 'center', marginBottom: '1rem' }}>
                 <h2>회원 추가</h2>
-            </div>
-            <FileUpload refreshFunction={updateImages} removefile={removefile} />
-            <Col md={{ offset: 4 }} style={{ width: '300px', height: '240px', borderRadius: '55%', border: '1px solid lightgray' }}>
-                {Images.map((image, index) => (
-                    <img key={index} style={{ minWidth: '300px', width: '300px', height: '240px', borderRadius: '55%', border: '1px solid lightgray' }} src={`${image}`} />
-                ))}
-            </Col>
-            <Form onSubmit={submitHandler} name="myform" data-testid="form">
-                <label>이름</label>
-                <Input type="text" onChange={onChange} placeholder={'이름 정보를 입력해주세요.'} value={form.name} name="name" data-testid="add-name" />
-                <br />
-                <label>카메라 기종</label>
-                <Input type="text" onChange={onChange} placeholder={'카메라 정보를 입력해주세요.'} value={form.camera} name="camera" data-testid="add-camera" />
-                <br />
-                <label>나이</label>
-                <Input type="number" onChange={onChange} placeholder={'나이 정보를 입력해주세요.'} value={form.age} name="age" data-testid="add-age" />
-                <br />
-                <select onChange={onChange} value={form.sex} name="sex" data-testid="add-sex">
-                    <option value="">성별을 선택해주세요</option>
-                    {Sex.map((item) => (
-                        <option key={item.key} value={item.key} data-testid="select-option">
-                            {item.value}
-                        </option>
-                    ))}
-                </select>
-                <Button type="submit" className="col-md-2 offset-md-10" onClick={submitHandler} data-testid="add-submit">
-                    확인
-                </Button>
-            </Form>
-        </div>
+            </div> */}
+            <Row>
+                <Col>
+                    <Card>
+                        <CardHeader>참고 사항</CardHeader>
+                        <S.card>
+                            <CardTitle tag="h5">※ 회원 추가 안내</CardTitle>
+                            <br />
+                            <br />
+                            <CardText>1. 앞으로 동호회에서 활동 예정인 동호회 회원분들의 정보를 추가해주세요.</CardText>
+                            <br />
+                            <CardText>2. 추가하실 때 회원 분의 이름, 성별, 나이, 보유 카메라, 프로필 사진을 꼭 추가해주세요.</CardText>
+                            <br />
+                            <CardText>3. 회원분이 동호회에서 탈퇴하거나 경고 누적으로 제명이 되면 회원 리스트 페이지에서 회원 정보를 삭제할 수 있습니다.</CardText>
+                            <S.pngImg src={img} />
+                        </S.card>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <CardHeader>회원 정보 추가</CardHeader>
+                        <S.card>
+                            <S.Profile>
+                                {Images.map((image, index) => (
+                                    <S.Img key={index} src={`${image}`} />
+                                ))}
+                                {Images.length === 0 ? <S.PersonIcon/> : ''}
+                            </S.Profile>
+                            <S.FileUpload>
+                                <FileUpload refreshFunction={updateImages} removefile={removefile} />
+                            </S.FileUpload>
+                            <Form onSubmit={submitHandler} name="myform" data-testid="form">
+                                <label>이름</label>
+                                <Input type="text" onChange={onChange} placeholder={'이름 정보를 입력해주세요.'} value={form.name} name="name" data-testid="add-name" />
+                                <br />
+                                <label>카메라 기종</label>
+                                <Input type="text" onChange={onChange} placeholder={'카메라 정보를 입력해주세요.'} value={form.camera} name="camera" data-testid="add-camera" />
+                                <br />
+                                <label>나이</label>
+                                <Input type="number" onChange={onChange} placeholder={'나이 정보를 입력해주세요.'} value={form.age} name="age" data-testid="add-age" />
+                                <br />
+                                <select onChange={onChange} value={form.sex} name="sex" data-testid="add-sex">
+                                    <option value="">성별을 선택해주세요</option>
+                                    {Sex.map((item) => (
+                                        <option key={item.key} value={item.key} data-testid="select-option">
+                                            {item.value}
+                                        </option>
+                                    ))}
+                                </select>
+                                <S.button color={'#54C5A0'} type="submit" className="col-md-2 offset-md-10 mt-1" onClick={submitHandler} data-testid="add-submit">
+                                    확인
+                                </S.button>
+                            </Form>
+                        </S.card>
+                    </Card>
+                </Col>
+            </Row>
+        </>
     );
 };
 
---
+++ client/src/components/Main/AddMemberPage/AddMemberPage.style.js
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+import { Col, CardBody, Button } from 'reactstrap';
+import { BsPersonFill, BsUpload, BsTrash } from 'react-icons/bs';
+
+const PersonIcon = styled(BsPersonFill)`
+    width: 300px;
+    height: 240px;
+    color: lightgrey;
+`;
+const uploadIcon = styled(BsUpload)`
+    color: white;
+    margin-bottom: 5px;
+    margin-right: 5px;
+`;
+const deleteIcon = styled(BsTrash)`
+    color: white;
+    margin-bottom: 5px;
+    margin-right: 5px;
+`;
+
+const button = styled(Button)`
+    margin-right: 10px;
+    font-weight: bold;
+    color: white;
+    background-color: ${(props) => props.color};
+
+    &:hover {
+        color: white;
+        background-color: lightslategray;
+    }
+`;
+const Profile = styled.div`
+    width: 300px;
+    height: 240px;
+    border-radius: 55%;
+    border: 1px solid lightgray;
+    margin-bottom: 15px;
+    display: block;
+    margin: 0px auto;
+    text-align: center;
+`;
+
+const Img = styled.img`
+    width: 300px;
+    height: 240px;
+    border-radius: 55%;
+    border: 1px solid lightgray;
+`;
+
+const FileUpload = styled.div`
+    margin-top: 30px;
+    margin-bottom: 30px;
+`;
+
+const pngImg = styled.img`
+    width: 400px;
+    height: 432px;
+    display: block;
+    margin: 0px auto;
+    @media only screen and (max-width: 500px) {
+        width: 300px;
+        height: 332px;
+    }
+`;
+
+const card = styled(CardBody)`
+    height:  720px;
+`;
+export { Profile, Img, FileUpload, pngImg, card, deleteIcon, PersonIcon, uploadIcon, button };
