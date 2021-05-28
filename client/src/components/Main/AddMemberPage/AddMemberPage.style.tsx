0199e8e7bf04c489b487b9064415ac0ec9bcd2f0
k0502s <k0502s@naver.com>
2021-05-28 12:29:57 +0900

    AddMemberPage 컴포넌트 타입스크립트 적용

---
+++ client/src/components/Main/AddMemberPage/AddMemberPage.style.tsx
@@ -0,0 +1,89 @@
+import styled from 'styled-components';
+import { Row, Card, Button } from 'reactstrap';
+import { BsPersonFill, BsUpload, BsTrash } from 'react-icons/bs';
+
+// Icon CSS
+
+const PersonIcon = styled(BsPersonFill)`
+    width: 300px;
+    height: 240px;
+    color: lightgrey;
+`;
+const UploadIcon = styled(BsUpload)`
+    color: white;
+    margin-bottom: 5px;
+    margin-right: 5px;
+`;
+const DeleteIcon = styled(BsTrash)`
+    color: white;
+    margin-bottom: 5px;
+    margin-right: 5px;
+`;
+
+// Add page CSS
+
+const Addbtn = styled(Button)`
+    margin-right: 10px;
+    font-weight: bold;
+    color: white;
+    background-color: ${(props) => props.color};
+    &:hover {
+        color: white;
+        background-color: lightslategray;
+    }
+`;
+const Profile = styled.div`
+    width: 300px;
+    height: 240px;
+    border-radius: 55%;
+    border: 1px solid lightgray;
+    margin-bottom: 15px;
+    display: block;
+    margin: 0px auto;
+    text-align: center;
+    @media only screen and (max-width: 360px) {
+        margin-left: -10px;
+    }
+`;
+
+const Img = styled.img`
+    width: 300px;
+    height: 240px;
+    border-radius: 55%;
+    border: 1px solid lightgray;
+`;
+
+const FileUpload = styled.div`
+    margin-top: 30px;
+    margin-bottom: 30px;
+`;
+
+const PngImg = styled.img`
+    width: 400px;
+    height: 432px;
+    display: block;
+    margin: 0px auto;
+    @media only screen and (max-width: 767px) {
+        width: 300px;
+        height: 332px;
+    }
+`;
+
+const AddCard = styled(Card)`
+    height: 790px;
+    background-color: #f4fcfb;
+    border-radius: 10px;
+    @media only screen and (max-width: 767px) {
+        margin-top: 10px;
+    }
+    @media only screen and (min-width: 768px) and (max-width: 1023px) {
+        margin-top: 10px;
+    }
+`;
+const Title = styled(Row)`
+    text-align: center;
+    display: block;
+    margin: -10px 0 18px 0;
+    color: #f9e81c;
+`;
+export { Profile, Img, FileUpload, PngImg, AddCard, DeleteIcon, PersonIcon, UploadIcon, Addbtn, Title };
---
+++ client/src/components/Main/AddMemberPage/AddMemberPage.tsx
@@ -0,0 +1,181 @@
+import React, { useState, useEffect } from 'react';
+import { Helmet } from 'react-helmet';
+import { Form, Input, CardTitle, CardText, Row, Col, CardHeader, CardBody } from 'reactstrap';
+import FileUpload from '../../../utils/FileUpload';
+import { Loader } from '../../Loader/Loader';
+import { useDispatch, useSelector } from 'react-redux';
+import img from '../../../assets/img/imgbin_photography-logo-photographer-png.png';
+import { MEMBER_UPLOADING_REQUEST, CLEAR_ERROR_REQUEST, CLEAR_ERROR_REQUEST_1 } from '../../../redux/types';
+import * as S from './AddMemberPage.style';
+
+type State = {
+    auth: {
+        user: {
+            _id: number;
+        };
+    };
+    member: {
+        isLoading2: boolean;
+    };
+};
+
+const SexInfo = [
+    { key: 1, value: '남' },
+    { key: 2, value: '여' },
+];
+
+const AddMemberPage = () => {
+    const dispatch = useDispatch();
+    const [form, setValues] = useState({
+        name: '',
+        camera: '',
+        age: '',
+    });
+    const [Sex, setSex] = useState({
+        sex: 0,
+    });
+
+    const [Images, setImages] = useState<any>([]);
+    const { user } = useSelector((state: State) => state.auth);
+    const { isLoading2 } = useSelector((state: State) => state.member);
+
+    useEffect(() => {
+        dispatch({
+            type: CLEAR_ERROR_REQUEST,
+        });
+        dispatch({
+            type: CLEAR_ERROR_REQUEST_1,
+        });
+    }, []);
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValues({
+            ...form,
+            [e.target.name]: e.target.value,
+        });
+    };
+    const onChangeSex = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setSex({
+            ...Sex,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (form.name === '') {
+            return alert('이름 정보를 입력해야 합니다.');
+        }
+        if (form.camera === '') {
+            return alert('카메라 기종 정보를 입력해야 합니다.');
+        }
+        if (form.age === '') {
+            return alert('나이 정보를 입력해야 합니다.');
+        }
+        if (Sex.sex === 0) {
+            return alert('성별 정보를 입력해야 합니다.');
+        }
+        if (Images.length === 0) {
+            return alert('프로필 사진을 입력해야 합니다.');
+        }
+
+        const { name, camera, age } = form;
+        const { sex } = Sex;
+
+        const body = {
+            writer: user._id,
+            name: name,
+            camera: camera,
+            age: age,
+            sex: sex,
+            images: Images,
+        };
+
+        dispatch({
+            type: MEMBER_UPLOADING_REQUEST,
+            payload: body,
+        });
+    };
+    const updateImages = (newImages: string[]) => {
+        setImages([...Images, newImages]);
+    };
+
+    const removefile = () => {
+        setImages([]);
+    };
+
+    const Body = (
+        <>
+            <S.Title>
+                <h1>ADD MEMBER</h1>
+            </S.Title>
+            <hr />
+            <Row>
+                <Helmet title={`회원 추가`} />
+                <Col>
+                    <S.AddCard>
+                        <CardHeader>
+                            <strong>참고 사항</strong>
+                        </CardHeader>
+                        <CardBody>
+                            <CardTitle tag="h5">※ 회원 추가 안내</CardTitle>
+                            <br />
+                            <br />
+                            <CardText>1. 앞으로 동호회에서 활동 예정인 동호회 회원분들의 정보를 추가해주세요.</CardText>
+                            <br />
+                            <CardText>2. 추가하실 때 회원 분의 이름, 성별, 나이, 보유 카메라, 프로필 사진을 꼭 추가해주세요.</CardText>
+                            <br />
+                            <CardText>3. 회원분이 동호회에서 탈퇴하거나 경고 누적으로 제명이 되면 회원 리스트 페이지에서 회원 정보를 삭제할 수 있습니다.</CardText>
+                            <S.PngImg src={img} />
+                        </CardBody>
+                    </S.AddCard>
+                </Col>
+                <Col>
+                    <S.AddCard>
+                        <CardHeader>
+                            <strong>회원 정보 추가</strong>
+                        </CardHeader>
+                        <CardBody>
+                            <S.Profile>
+                                {Images.map((image: string, index: React.Key) => (
+                                    <S.Img key={index} src={`${image}`} />
+                                ))}
+                                {Images.length === 0 ? <S.PersonIcon /> : ''}
+                            </S.Profile>
+                            <S.FileUpload>
+                                <FileUpload refreshFunction={updateImages} removefile={removefile} />
+                            </S.FileUpload>
+                            <Form onSubmit={submitHandler} name="myform" data-testid="form">
+                                <label>이름</label>
+                                <Input type="text" onChange={onChange} placeholder={'이름 정보를 입력해주세요.'} value={form.name} name="name" data-testid="add-name" />
+                                <br />
+                                <label>카메라 기종</label>
+                                <Input type="text" onChange={onChange} placeholder={'카메라 정보를 입력해주세요.'} value={form.camera} name="camera" data-testid="add-camera" />
+                                <br />
+                                <label>나이</label>
+                                <Input type="number" onChange={onChange} placeholder={'나이 정보를 입력해주세요.'} value={form.age} name="age" data-testid="add-age" />
+                                <br />
+                                <select onChange={onChangeSex} value={Sex.sex} name="sex" data-testid="add-sex">
+                                    <option value="">성별을 선택해주세요</option>
+                                    {SexInfo.map((item) => (
+                                        <option key={item.key} value={item.key} data-testid="select-option">
+                                            {item.value}
+                                        </option>
+                                    ))}
+                                </select>
+                                <S.Addbtn color={'#54C5A0'} type="submit" className="col-md-2 offset-md-10 mt-1" onClick={submitHandler} data-testid="add-submit">
+                                    확인
+                                </S.Addbtn>
+                            </Form>
+                        </CardBody>
+                    </S.AddCard>
+                </Col>
+            </Row>
+        </>
+    );
+
+    return <>{isLoading2 ? Loader : Body}</>;
+};
+
+export default AddMemberPage;
